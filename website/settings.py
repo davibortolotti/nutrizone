04d13a9e213666649d99a26fca4f717fb731169d
Bortolotti <davibortolotti@gmail.com>
2017-08-01 09:07:25 -0300

    fixes static once agin

--- website/settings.py
+++ website/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'nutrizone/static')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
