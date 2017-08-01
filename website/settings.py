7f30d3e354e13304af1ada52de804db761ea0a51
Bortolotti <davibortolotti@gmail.com>
2017-08-01 09:09:30 -0300

    fixes static once agin

--- website/settings.py
+++ website/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'nutrizone/static'),
 )
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
