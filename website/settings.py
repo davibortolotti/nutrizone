c7f3272d09ada2e39ddf9c7d2659088e2e832957
Bortolotti <davibortolotti@gmail.com>
2017-08-01 10:58:46 -0300

    changes in layout to responsive

--- website/settings.py
+++ website/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'nutrizone/static'),
+    os.path.join(BASE_DIR, 'nutrizone/staticfiles'),
 )
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
