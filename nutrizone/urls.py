55b7da06555f07b414ace2d3125ed030cd737e4e
Bortolotti <davibortolotti@gmail.com>
2017-08-01 09:39:47 -0300

    changes urls for static

--- nutrizone/urls.py
+++ nutrizone/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -10,4 +12,4 @@ urlpatterns = [
 	url(r'^meal$', views.meal, name='meal'),
 	url(r'^alimentos$', views.foodlist, name='foodlist'),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
